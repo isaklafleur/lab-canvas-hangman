b3bbb6e809c45e1323fc252d3f2aaae5dae86032
Isak La Fleur <isak.engdahl@gmail.com>
2017-10-25 00:07:09 +0200

    fixed some bugs. Now the logic is almost there for the Class Hangman.

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,87 @@
 var hangman;
 
 function Hangman() {
-
+  this.words = ["brazil", "spain", "germany"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 5;
+  this.messages = {
+    win: "You won!",
+    loose: "Game over!",
+    guessed: "You already guessed this letter, please try again..",
+    notValidLetter: "Please enter a valid letter from a-z",
+  };
 }
 
-Hangman.prototype._getWord = function () {
-
+Hangman.prototype._getWord = function() {
+  return this.words[Math.floor(Math.random() * this.words.length)];
 };
 
 Hangman.prototype._checkIfLetter = function(keyCode) {
-
+  return /^[a-zA-Z]*$/.test(String.fromCharCode(keyCode));
 };
 
 Hangman.prototype._checkClickedLetters = function(key) {
-
+  return !this.letters.includes(key);
 };
 
-Hangman.prototype._addCorrectLetter = function(i){
-
+Hangman.prototype._addCorrectLetter = function(key) {
+  if (this.secretWord.includes(key)) {
+    console.log(`${key} is part of the secret word`);
+    this.guessedLetter += key;
+    if (this._checkWinner()) {
+      console.log(this.messages.win);
+    }
+  } else {
+    console.log(`${key} is NOT part of the secret word`);
+    this._addWrongLetter();
+    if (this._checkGameOver()) {
+      console.log(this.messages.loose);
+    }
+  }
 };
 
-Hangman.prototype._addWrongLetter = function (letter){
-
+Hangman.prototype._addWrongLetter = function(letter) {
+  return this.secretWord.includes(letter)
+    ? this.errorsLeft
+    : (this.errorsLeft -= 1);
 };
 
 Hangman.prototype._checkGameOver = function() {
-
+  return this.errorsLeft === 0;
 };
 
 Hangman.prototype._checkWinner = function() {
-
+  return (
+    this.secretWord
+      .split("")
+      .sort()
+      .join("") ===
+    this.guessedLetter
+      .split("")
+      .sort()
+      .join("")
+  );
 };
 
-document.getElementById("start-game-button").onclick = function(){
+document.getElementById("start-game-button").onclick = function() {
   hangman = new Hangman();
+  hangman.secretWord = hangman._getWord();
+  console.log(hangman.secretWord);
 };
-
 
 document.onkeydown = function(e) {
-
+  if (hangman != undefined) {
+    if (hangman._checkIfLetter(e.keyCode)) {
+      if (hangman._checkClickedLetters(e.key)) {
+        hangman.letters.push(e.key);
+        hangman._addCorrectLetter(e.key);
+      } else {
+        console.log(hangman.messages.guessed);
+      }
+    } else {
+      console.log(hangman.messages.notValidLetter);
+    }
+  }
 };
